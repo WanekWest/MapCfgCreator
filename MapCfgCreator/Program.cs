10c74ce091bad8e0cfaea526ecdb874f3ea07488
WanekWest <broviva@mail.ru>
2022-03-28 22:39:06 +0300

    Добавьте файлы проекта.

---
+++ MapCfgCreator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace MapCfgCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilesNames = Console.ReadLine();
+            string[] FileList = FilesNames.Split(';');
+            string Path = Console.ReadLine();
+
+            foreach (var File in FileList)
+            {
+                FileInfo fileInf = new FileInfo(@$"{Path}\{File}");
+                if (!fileInf.Exists)
+                {
+                    fileInf.Create();
+                }
+            }
+        }
+    }
+}
